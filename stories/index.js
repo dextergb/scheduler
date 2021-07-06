11b6d4e60b1c95f3a5fb0ed0e142c5139e404e74
dextergb <dextergb@rogers.com>
2021-07-06 01:18:38 -0400

    change the button component to render a button element

--- stories/index.js
+++ stories/index.js
@@ -7,9 +7,15 @@ import "index.scss";
 
 import Button from "components/Button";
 
+// Base uses no props and is considered the default button
+// Confirm uses the confirm prop to apply the .button--confirm modifier class
+// Danger uses the danger prop to apply the .button--danger modifier class
+// Clickable uses the onClick prop to handle the button click event
+// Disabled uses the disabled prop to apply the disabled attribute to the button element
+
 storiesOf("Button", module)
   .addParameters({
-    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
   })
   .add("Base", () => <Button>Base</Button>)
   .add("Confirm", () => <Button confirm>Confirm</Button>)
