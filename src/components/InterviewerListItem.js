b5dfda0e4a5f67149400394bbb85c03864051994
dextergb <dextergb@rogers.com>
2021-07-06 23:44:47 -0400

    Make changes to the InterviewerList and InterviewerListItem components so that we are no longer passing the id prop on the InterviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -5,16 +5,12 @@ import "components/InterviewerListItem.scss";
 import classnames from "classnames";
 
 export default function InterviewerListItem(props) {
-  const interviewerListItemClass = classnames("interviewers__item", {
+  const interviewerClass = classnames("interviewers__item", {
     "interviewers__item-image": props.avatar,
     "interviewers__item--selected": props.selected,
   });
   return (
-    <li
-      className={interviewerListItemClass}
-      onClick={() => props.setInterviewer(props.name)}
-      key={props.id}
-    >
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
