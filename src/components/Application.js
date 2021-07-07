bc57595e38382a25c67c041e77ed5c0745e754e3
dextergb <dextergb@rogers.com>
2021-07-07 16:22:14 -0400

    Add appointments test data

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,53 @@ import "components/Application.scss";
 
 import DayList from "./DayList";
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  {
+    id: 3,
+    time: "3pm",
+    interview: {
+      student: "Andy B",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+  {
+    id: 4,
+    time: "4:30pm",
+  },
+  {
+    id: 5,
+    time: "5pm",
+    interview: {
+      student: "Comet Z",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      },
+    },
+  },
+];
+
 const days = [
   {
     id: 1,
