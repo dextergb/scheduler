88414b184eb6abd88947d69c903990b816ac33f7
dextergb <dextergb@rogers.com>
2021-07-06 21:21:32 -0400

    Update InterviewList component using values from props in code

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,13 +2,23 @@ import React from "react";
 
 import "components/InterviewerList.scss";
 
-import classnames from "classnames";
+import InterviewerListItem from "./InterviewerListItem";
 
-export default function InterviewerListItem(props) {
+export default function InterviewerList(props) {
+  const { interviewers } = props;
+  const parsedInterviewList = interviewers.map((interview) => (
+    <InterviewerListItem
+      key={interview.id}
+      name={interview.name}
+      avatar={interview.avatar}
+      selected={interview.id === props.interviewer}
+      setInterviewer={props.setInterviewer}
+    />
+  ));
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{parsedInterviewList}</ul>
     </section>
   );
 }
