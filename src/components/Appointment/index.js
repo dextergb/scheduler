dc71f55bf2c321c7541ae2756d1ce091305578ea
dextergb <dextergb@rogers.com>
2021-07-07 03:26:57 -0400

    Import Header, Show and Empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,10 @@ import React from "react";
 
 import "components/Appointment/styles.scss";
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 export default function Appointment(props) {
   return <article className="appointment"></article>;
 }
