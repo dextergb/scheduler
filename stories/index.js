74182b79aa927d1c208e5160714d530368f88f45
dextergb <dextergb@rogers.com>
2021-07-07 01:47:12 -0400

    Import Appointment/Status

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 // Base uses no props and is considered the default button
 // Confirm uses the confirm prop to apply the .button--confirm modifier class
