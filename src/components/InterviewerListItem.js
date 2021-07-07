c47e78454d1e8ec5bc5460649efd54f46f18b475
dextergb <dextergb@rogers.com>
2021-07-06 21:13:15 -0400

    Update component by using values from props

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -2,15 +2,25 @@ import React from "react";
 
 import "components/InterviewerListItem.scss";
 
+import classnames from "classnames";
+
 export default function InterviewerListItem(props) {
+  const interviewerListItemClass = classnames("interviewers__item", {
+    "interviewers__item-image": props.avatar,
+    "interviewers__item--selected": props.selected,
+  });
   return (
-    <li className="interviewers__item">
+    <li
+      className={interviewerListItemClass}
+      onClick={() => props.setInterviewer(props.name)}
+      key={props.id}
+    >
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.selected && props.name}
     </li>
   );
 }
