dce15dace1cc44b72c633eac467465716598a362
dextergb <dextergb@rogers.com>
2021-07-08 17:07:04 -0400

    Make variable called dailyAppointments and update map function

--- src/components/Application.js
+++ src/components/Application.js
@@ -8,59 +8,61 @@ import Appointment from "components/Appointment";
 
 const axios = require("axios");
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  {
-    id: 3,
-    time: "3pm",
-    interview: {
-      student: "Archie Cohen",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-  {
-    id: 4,
-    time: "4:30pm",
-  },
-  {
-    id: 5,
-    time: "5pm",
-    interview: {
-      student: "Maria Boucher",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png",
-      },
-    },
-  },
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       },
+//     },
+//   },
+//   {
+//     id: 3,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Cohen",
+//       interviewer: {
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       },
+//     },
+//   },
+//   {
+//     id: 4,
+//     time: "4:30pm",
+//   },
+//   {
+//     id: 5,
+//     time: "5pm",
+//     interview: {
+//       student: "Maria Boucher",
+//       interviewer: {
+//         id: 2,
+//         name: "Tori Malcolm",
+//         avatar: "https://i.imgur.com/Nmx0Qxo.png",
+//       },
+//     },
+//   },
+// ];
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
+    appointments: {},
   });
 
+  const dailyAppointments = [];
   const setDay = (day) => setState({ ...state, day });
 
   useEffect(() => {
@@ -75,7 +77,7 @@ export default function Application(props) {
       });
   }, []);
 
-  const appointmentList = appointments.map((appointment) => (
+  const appointmentList = dailyAppointments.map((appointment) => (
     <Appointment key={appointment.id} {...appointment} />
   ));
   return (
