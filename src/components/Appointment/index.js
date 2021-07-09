7bc1bb0b5f0e910529da59429de6bf5fd2a239bc
dextergb <dextergb@rogers.com>
2021-07-09 03:00:33 -0400

    Implement modes EMPTY,SHOW,CREATE

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,9 +6,11 @@ import useVisualMode from "hooks/useVisualMode";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -17,11 +19,18 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onSave={() => console.log("Clicked onSave")}
+          onCancel={() => back()}
         />
       )}
     </article>
