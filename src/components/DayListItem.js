4274e84c27cbbb516a088983289c53922436b60b
dextergb <dextergb@rogers.com>
2021-07-13 14:40:59 -0400

    Fix to pass test

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,7 +9,7 @@ export default function DayListItem(props) {
   });
 
   const formatSpots = (spots) => {
-    if (spots === 0) return "No spots remaining";
+    if (spots === 0) return "no spots remaining";
     if (spots === 1) return "1 spot remaining";
     return `${spots} spots remaining`;
   };
