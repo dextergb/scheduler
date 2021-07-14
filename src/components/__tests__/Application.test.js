c4f943d191cb9853ccebca7cc416d6b8c7144e70
dextergb <dextergb@rogers.com>
2021-07-14 01:52:41 -0400

    Access the debug function returned by the render function

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -28,7 +28,7 @@ describe("Application", () => {
   });
 
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
