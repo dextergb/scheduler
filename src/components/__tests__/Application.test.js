cb49b2127ef0f59eabd814665789243d5287a462
dextergb <dextergb@rogers.com>
2021-07-14 01:31:34 -0400

    Use the waitForElement function, and wait until after the element containing 'Archie Cohen' renders. Use the async and await syntax learned near the beginning of the Easier Async so that we call console.log(prettyDOM(container)) after the data loads

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -24,8 +24,11 @@ describe("Application", () => {
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 
-  it("loads data, books an interview and reduces the spots remaining for Monday by 1", () => {
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
     const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
     console.log(prettyDOM(container));
   });
 });
