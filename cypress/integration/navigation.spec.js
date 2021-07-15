4a2bdbc5e1488fe29bab9794f3555dd1fd25ca90
dextergb <dextergb@rogers.com>
2021-07-15 00:46:22 -0400

    Complete navigating days test

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,11 @@
 describe("Navigation", () => {
-  it("should visit root", () => {
+  it("Should visit root", () => {
     cy.visit("/");
+  });
+  it("Should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 });
